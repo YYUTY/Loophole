ce5adab4c5066aff44dc5d9a2550a68938120c0c
YYUTY <asa2siriaru@gmail.com>
2022-11-02 00:18:16 +0900

    update

--- data/menubar.py
+++ data/menubar.py
@@ -9,7 +9,7 @@ class Menubar(tk.Menu):
 
     def file_menu(self):
         file_menu=tk.Menu(self, tearoff=0)
-        file_menu.add_command(label='New', command=lambda:self.commands[0]('OK'))
+        file_menu.add_command(label='New', command=lambda:self.commands[0]('     untitled     '))
         file_menu.add_command(label='Open', command=lambda:self.commands[1]())
         file_menu.add_command(label='Save', command=lambda:self.commands[2]())
         file_menu.add_command(label='Save As')
