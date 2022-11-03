77614103d1e204decc463714d864816129590360
YYUTY <asa2siriaru@gmail.com>
2022-11-03 19:04:42 +0900

    cs test

--- data/main.py
+++ data/main.py
@@ -2,17 +2,24 @@ import os
 import inspect
 import tkinter as tk
 from tkinter import ttk
+import subprocess as sp
 from . import menubar as menu
 from tkinter import filedialog
 from . import custom_style as cs
 
 class Application(tk.Frame):
     def __init__(self, master):
-        menubar=menu.Menubar(master, [self.add_tab,self.fileopen,self.filesave])
+        super().__init__(master,width=1280,height=800)
+        menubar=menu.Menubar(master, [self.add_tab, self.fileopen, self.filesave, self.filesaveas, self.run])
         master.title('Loophole')
-        master.geometry('640x480')
-        self.notebook=cs.CustomNotebook(width=200, height=200)
-        self.notebook.pack(fill='both',expand=1)
+        master.geometry('1280x800')
+        self.csc_path='C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\csc.exe'
+        self.notebook=cs.CustomNotebook(width=1050, height=600)
+        self.notebook.pack(anchor=tk.NE)
+        self.output_frame=tk.Frame(master,width=1050,height=200,bg='black')
+        self.output_frame.pack(padx=(225,0))
+        self.op=tk.Label(self.output_frame,fg='white',text='OooooooooooooooooooooooK',background='#0000aa')
+        self.op.pack(side=tk,LEFT)
         self.tframes=[]
         self.fnames=[]
 
@@ -20,7 +27,7 @@ class Application(tk.Frame):
         tframe=cs.SbTextFrame(self.notebook)
         self.tframes.append(tframe)
         if os.path.isfile(fname):
-            with open(fname,'r') as f:
+            with open(fname,'r', encoding='utf-8') as f:
                 lines=f.readlines()
             for line in lines:
                 tframe.text.insert('end',line)
@@ -37,10 +44,23 @@ class Application(tk.Frame):
         idx=self.notebook.tabs().index(self.notebook.select())
         fname=self.fnames[idx]
         tframe=self.tframes[idx]
-        with open(fname,'w') as f:
+        with open(fname,'w', encoding='utf-8') as f:
             f.writelines(tframe.text.get('1.0','end-1c'))
 
+    def filesaveas(self):
+        idx=self.notebook.tabs().index(self.notebook.select())
+        fname=filedialog.asksaveasfilename()
+        tframe=self.tframes[idx]
+        with open(fname,'w', encoding='utf-8') as f:
+            f.writelines(tframe.text.get('1.0','end-1c'))
+
+    def run(self):
+        idx=self.notebook.tabs().index(self.notebook.select())
+        fname=self.fnames[idx]
+        sp.run('{} {}'.format(self.csc_path,fname))
+        self.op['text']=sp.run([os.path.join(os.getcwd(),"{}.exe".format(os.path.splitext(fname)[0]))], check=True, shell=True, stdout=sp.PIPE, stderr=sp.PIPE ,encoding="utf-8")
+
 def main():
-    root = tk.Tk()
-    app = Application(root)
+    root=tk.Tk()
+    app=Application(root)
     root.mainloop()
--- data/menubar.py
+++ data/menubar.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from functools import partial
 
 class Menubar(tk.Menu):
     def __init__(self, master, commands):
@@ -7,10 +8,18 @@ class Menubar(tk.Menu):
         self.commands=commands
         self.file_menu()
 
+    def cm_func(self, n, event=None, text=None):
+        if text==None:
+            self.commands[n]()
+        else:
+            self.commands[n](text)
+
     def file_menu(self):
         file_menu=tk.Menu(self, tearoff=0)
-        file_menu.add_command(label='New', command=lambda:self.commands[0]('     untitled     '))
-        file_menu.add_command(label='Open', command=lambda:self.commands[1]())
-        file_menu.add_command(label='Save', command=lambda:self.commands[2]())
-        file_menu.add_command(label='Save As')
+        file_menu.add_command(label='New', command=lambda:self.cm_func(0,text='     untitled     '))
+        file_menu.add_command(label='Open', command=lambda:self.cm_func(1))
+        file_menu.add_command(label='Save', command=lambda:self.cm_func(2))
+        file_menu.add_command(label='Save As', command=lambda:self.cm_func(3))
         self.add_cascade(label="File", menu=file_menu)
+        self.bind_all('<Control-Shift-S>', partial(self.cm_func, 3))
+        self.bind_all('<Alt-r>', partial(self.cm_func, 4))
